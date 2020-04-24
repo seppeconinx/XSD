03ae6e9aadd707861285de3c8dc8bd5ee5ee439b
Ward Toulet <ward@toulet.net>
2020-04-24 15:22:52 +0200

    added tester

---
+++ docs/tester.js
@@ -0,0 +1,30 @@
+'use strict'
+const buttonValidate = document.getElementById('button--validate');
+const xmlString = document.getElementById('input--xmlstr');
+const inputSchema = document.getElementById('input--schema');
+const schemas = {
+	'general user': 'https://raw.githubusercontent.com/Anyvent/XSD/master/general_user.xsd'
+}
+
+// Load schemas
+for(const schema in schemas) {
+	const opt = document.createElement('option');
+	opt.innerText = schema;
+	inputSchema.appendChild(opt);
+}
+
+function validate() {
+	const schemaName = inputSchema.options[inputSchema.selectedIndex].value
+	const schemaUrl = schemas[schemaName];
+	const schema = getSchema(schemaUrl);
+	console.log(`validating ${xmlString.value}`);
+	console.log(`schema: ${schema}`);
+}
+
+async function getSchema(url) {
+	const response = await fetch(url);
+	const rawXML = await response.text();
+	return rawXML;
+}
+
+buttonValidate.addEventListener('click', validate);
